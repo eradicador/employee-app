2c3cb9f340643d19fafa1383e945240ca99d6943
eradicador <janisepinto@yahoo.com>
2020-08-10 04:53:04 -0400

    added code for the main page and header.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 //import react module
 import React from "react";
-//import the SearchResultContainer component 
-import SearchResultContainer from "./components/SearchResultContainer";
+//import the components for the main page 
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+// import "./App.css";
 //function that returns the component SearchResultContainer
 function App() {
-  return <SearchResultContainer />;
+  return (
+    <div className="App">
+      {/* <Wrapper> */}
+        <Header />
+        {/* <Main />
+      </Wrapper> */}
+    </div>
+  );
+
 }
 
 export default App;
