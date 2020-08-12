2d231b27b884be5ad3699ceb4acba3ba87763c75
eradicador <janisepinto@yahoo.com>
2020-08-12 10:00:40 -0400

    Updates

---
+++ precache-manifest.101887a8af03a13bd965e1e8912d2a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6803d185f297e86d608f5db603791ed8",
+    "url": "/employee-app/index.html"
+  },
+  {
+    "revision": "f73d4a12b4187d68f856",
+    "url": "/employee-app/static/css/main.2b749272.chunk.css"
+  },
+  {
+    "revision": "80cc01469d148f0a4a5b",
+    "url": "/employee-app/static/js/2.2d1f667e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-app/static/js/2.2d1f667e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73d4a12b4187d68f856",
+    "url": "/employee-app/static/js/main.d839a4c4.chunk.js"
+  },
+  {
+    "revision": "c3aab284ebc32d6bbdff",
+    "url": "/employee-app/static/js/runtime-main.37688bba.js"
+  }
+]);
